72fae63c68a1c26bdb20f74028dc35b825438416
wr <wagnerrafael.13@gmail.com>
2026-04-07 15:29:42 -0400

    Media de notas - while

--- atv.js
+++ atv.js
@@ -1,4 +1,4 @@
-const r = require('readline-sync');
+/* const r = require('readline-sync');
 
 let nota; 
 
@@ -10,4 +10,23 @@ do {
 
 }while (nota < 0 || nota > 10);
 
-console.log("Nota válida: " + nota);
+console.log("Nota válida: " + nota); */
+
+const r = require('readline-sync');
+
+let soma=0;
+let nota;
+let contador = 1;
+
+while (contador <= 5){
+    nota = parseFloat(r.question('Digite a nota ' + contador + ': '));
+    if (nota >= 0 && nota <= 10) {
+    soma += nota;
+    contador++;    
+    }else {
+        console.log("Nota inválida. Digite outra nota.");
+    }
+}
+let media = soma / 5;
+console.log("A soma das notas é: " + soma);
+console.log("A média das notas é: " + media);
